e834b455adda150aaac90201c5579620c7cae945
AnZave <crossya94@gmail.com>
2024-07-23 01:39:37 +0300

    fix tbody

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -9,16 +9,15 @@ const TransactionHistory = ({ items }) => {
           <th>Currency</th>
         </tr>
       </thead>
-
-      {items.map((item) => (
-        <tbody key={item.id}>
-          <tr className={styles.item}>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id} className={styles.item}>
             <td>{item.type}</td>
             <td>{item.amount}</td>
             <td>{item.currency}</td>
           </tr>
-        </tbody>
-      ))}
+        ))}
+      </tbody>
     </table>
   );
 };
